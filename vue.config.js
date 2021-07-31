f5c660ae1a8267f24736de22b27b39812907624f
wengwj-web <386010929@qq.com>
2021-07-31 14:55:36 +0800

    编写header

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        prependData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/music-next/' : '/'
+}
